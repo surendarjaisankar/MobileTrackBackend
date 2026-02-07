e3a37d66a0cd26a8a895ac9602bfde52fc401716
Surendar Jaisankar <125639407+surendarjaisankar@users.noreply.github.com>
2026-02-07 10:56:05 +0530

    Update authRoutes.js

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,8 +1,1 @@
-const express = require('express');
-const { login } = require('../controllers/authController');
 
-const router = express.Router();
-
-router.post('/login', login);
-
-module.exports = router;
