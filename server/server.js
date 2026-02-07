8f09182cf23e41d94e2cd21fb20244678129c2fa
Surendar Jaisankar <125639407+surendarjaisankar@users.noreply.github.com>
2026-02-07 09:28:29 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const server = http.createServer(app)
 // CORS CONFIG
 // ----------------------
 const allowedOrigin =
-  process.env.FRONTEND_URL || 'http://localhost:3000'
+  process.env.FRONTEND_URL;
 
 app.use(
   cors({
@@ -100,7 +100,7 @@ mongoose
 // ----------------------
 // START SERVER
 // ----------------------
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
